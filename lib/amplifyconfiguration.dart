84c967e217ececb1cf5fd59baf018e3461ced3cb
taufiqzaidong <taufiqzaidong@gmail.com>
2020-12-30 01:33:03 +0800

    first commit

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:f042ef37-ce2a-48db-a32c-a751736452de",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_yVg3zjhzp",
+                        "AppClientId": "3i11pp4usq8siotboufj5q5iai",
+                        "AppClientSecret": "5j55ijspsnf8e6sghial4tej55i7kqm09b3e3s55ilek7iidihu",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
